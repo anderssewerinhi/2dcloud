8bb2fc1b4d23d12a651dfa98005383a94261b0ed
anderssewerinhi <anders.johansen@humaninference.com>
2014-01-24 10:21:22 +0100

    Added a simple log4j config file in the root

--- src/main/java/com/humaninference/tagcloud/rmi/worldofwords/runnables/RunMasterAndSlaveOnLocalhost.java
+++ src/main/java/com/humaninference/tagcloud/rmi/worldofwords/runnables/RunMasterAndSlaveOnLocalhost.java
@@ -1,12 +1,21 @@
 package com.humaninference.tagcloud.rmi.worldofwords.runnables;
 
+import java.io.File;
 import java.rmi.AlreadyBoundException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
+import org.apache.log4j.xml.DOMConfigurator;
+
 public class RunMasterAndSlaveOnLocalhost {
 
 	public static void main(String[] args) throws RemoteException, AlreadyBoundException, NotBoundException {
+		if (new File("log4j-configure.xml").exists()) {
+			DOMConfigurator.configure("log4j-configure.xml");
+		} else {
+			final String currentDir = new File(".").getAbsolutePath();
+			System.out.println("No log4j config file found in " + currentDir);
+		}
 		MasterNode.main("1");
 		ClientNode.main("localhost");
 	}
